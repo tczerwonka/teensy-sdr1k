436a928a2bb18b2fb6cbb7ef9e6446781918f6c2
T Czerwonka <tczerwonka@gmail.com>
2024-01-01 20:54:37 -0600

    some test code for reading values

--- test0/teensy-sdr1k-test0/sdr1k_pcf.h
+++ test0/teensy-sdr1k-test0/sdr1k_pcf.h
@@ -5,24 +5,50 @@
 #ifndef SDR1K_PCF_H
 #define SDR1K_PCF_H
 
+	static const int PCF_ADDR = 0x20;
+
+  static const int PO0 = 0x00;
+  static const int PO1 = 0x01;
+  static const int PO2 = 0x02;
+  static const int PO3 = 0x03;
+  static const int PO4 = 0x04;
+  static const int PO5 = 0x05;
+  static const int PO6 = 0x06;
+  static const int PO7 = 0x07;
+  static const int PI0 = 0x08;
+  static const int PI1 = 0x09;
+  static const int PI2 = 0x0A;
+  static const int PI3 = 0x0B;
+  static const int PI4 = 0x0C;
+  static const int PI5 = 0x0D;
+  static const int PI6 = 0x0E;
+  static const int PI7 = 0x0F;
+
 	//SDR	PCF
-	//C0	PI0	-- PIO - X2 output / also K2 TRX gain
-	//D0	PO0
-	//D1	PO1
-	//D2	PO2
-	//D3	PO3
-	//D4	PO4
-	//D5	PO5
-	//D6	PO6
-	//D7	PO7	
+	static const int C0	= PI0;	// PIO - X2 output / also K2 TRX gain
+	static const int D0 =	PO0;
+	static const int D1 = PO1;
+	static const int D2 = PO2;
+	static const int D3 = PO3;
+	static const int D4 = PO4;
+	static const int D5 = PO5;
+	static const int D6 = PO6;
+	static const int D7 = PO7;
 	
-	//S7	PI5	-- EXT X2-11 -- maybe input for ADC, etc?
-	//S5	PI7	-- dot / ptt
-	//S4	PI4	-- dash
-	//C1	PI1	-- BPF PIO
-	//S3	PI6 	-- EXT X2-12 -- maybe input for ADC, etc?
-	//C2	PI2	-- DAT - DDS0 / IC11
-	//C3	PI3	-- ADR - DDS1 / IC8
+	static const int C1 = PI1;	// BPF PIO
+	static const int C2 = PI2;	// DAT - DDS0 / IC11
+	static const int C3 = PI3;	// ADR - DDS1 / IC8
+
+  //these are all input
+ 	static const int S3 = PI6;  // EXT X2-12 -- maybe input for ADC, etc?
+ 	static const int S4 = PI4;	// dash
+ 	static const int S5 = PI7;	// dot / ptt
+  //no S6 -- is logic S4 || S5 -- for parallel interupt -- I don't care
+	static const int S7 = PI5;	// EXT X2-11 -- maybe input for ADC, etc?
+
+
+
+
 	
 	
 	
--- test0/teensy-sdr1k-test0/teensy-sdr1k-test0.ino
+++ test0/teensy-sdr1k-test0/teensy-sdr1k-test0.ino
@@ -1,9 +1,46 @@
+//==============================================================================
+// teensy-sdr1k-test0
+//==============================================================================
+// very basic code to see if I can do anything at all with the PCF8575
+// using the following library:
+//  https://github.com/RobTillaart/PCF8575
+//==============================================================================
+
+#include "hardware_v2.h"
+#include "sdr1k_pcf.h"
+
+#include <PCF8575.h>
+
+
+PCF8575 PCF(PCF_ADDR);
+
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println(__FILE__);
+  Serial.print("PCF8575_LIB_VERSION:\t");
+  Serial.println(PCF8575_LIB_VERSION);
+
+  Wire.begin();
+
+  PCF.begin();
+
+  //test of what is returned by read16
+  //appears by default all pins are pulled high
+  //without input all are reading 1 -- tied to ground -- read 0
+  PCF.selectNone(); // set all pins low
+  uint16_t value = PCF.read16();
+  Serial.print("#bin:\t");
+  Serial.println(value, BIN);
+  //PCF8575_LIB_VERSION:	0.2.1
+  //#bin:	110111111111111
+  //PI7 PI6 PI5...PI0 PO7...PO0
+
+
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print("S3/PI6:");
+  Serial.println(PCF.readButton(S3));
+  delay(1000);
 }
